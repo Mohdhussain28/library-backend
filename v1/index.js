51387d4b45630d37436a8f2e88dfb4e9eb6fe252
Mohdhussain28 <mohdhussain28@outlook.com>
2024-09-28 14:08:32 +0530

    database connected

--- v1/index.js
+++ v1/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const cors = require('cors');
 const bodyparser = require('body-parser');
-
+const ckey = require('ckey')
+const connectDB = require('./config/database');
+// const bookRoutes = require('./routes/bookRoutes');
+const userRoutes = require('./routes/userRoutes');
+// const transactionRoutes = require('./routes/transactionRoutes');
 
 const app = express();
 
 app.use(cors({ origin: true }))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
+
+connectDB();
+
+// app.use('/books', bookRoutes);
+app.use('/users', userRoutes);
+// app.use('/transactions', transactionRoutes);
+
+const PORT = ckey.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
